b867601130c1c3b8b2d3981c243a323e03924aa1
Richard Anthony <public.rant@pm.me>
2026-04-07 09:06:01 +1000

    style: apply Investigative Dossier design system to astro site and Storybook

--- src/stories/editorial/storyFrame.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { Decorator } from '@storybook/react-vite';
-
-export const withEditorialFrame: Decorator = (Story) => (
-	<div className="story-frame">
-		<Story />
-	</div>
-);
